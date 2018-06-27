d61b6d574489a4bf6b62d170abdec469037ec602
Maggiexuiscute <maggiexuiscute@gmail.com>
2018-06-27 15:43:01 +0800

    Assets added

---
+++ Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+	public GameObject target; 
+
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+
+
+	void OnMouseDown () {
+
+		LookAtTarget.target = target;
+        Camera.main.fieldOfView = Mathf.Clamp(60 * target.transform.localScale.x, 1, 100);
+	}
+}
